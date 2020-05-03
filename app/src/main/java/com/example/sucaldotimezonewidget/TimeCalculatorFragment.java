441c267753792326d49afb27fcce19188b1cda10
Sofia Calderon <sofia.ester.calderon@gmail.com>
2020-05-03 11:14:16 +1000

    Added barebone layout of time calculator

--- app/src/main/java/com/example/sucaldotimezonewidget/TimeCalculatorFragment.java
+++ app/src/main/java/com/example/sucaldotimezonewidget/TimeCalculatorFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -13,45 +14,47 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-//public class TimeCalculatorFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+public class TimeCalculatorFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
-public class TimeCalculatorFragment extends Fragment {
-
-    // Definition of variables
-    // Test2
     private Spinner spinner;
     private ArrayAdapter<CharSequence> adapter;
+    private Button calculateBtn;
+
+    private String selectedCity;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-                 // Link xml layout file with this activity (fragment)
-                View rootView = inflater.inflate(R.layout.fragment_time_calculator_dummy,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_time_calculator, container, false);
 
-//                // Assign xml object to variable
-//                spinner = (Spinner) rootView.findViewById(R.id.spinner);
-//                // give my string-array "cities" a pre-defined android array layout "simple_spinner_item"
-//                adapter =  ArrayAdapter.createFromResource(getActivity(),R.array.cities,android.R.layout.simple_spinner_item);
-//                // set dropdown-menu
-//                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//                // Link everything to spinner
-//                spinner.setAdapter(adapter);
-//
-//
-//                // Define listener for menu items
-//                spinner.setOnItemSelectedListener(this);
-//
+        spinner = rootView.findViewById(R.id.spinner);
+        adapter = ArrayAdapter.createFromResource(getActivity(), R.array.cities, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        calculateBtn = rootView.findViewById(R.id.calculate_btn);
+        calculateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculateTime();
+            }
+        });
+
         return rootView;
     }
 
-//    @Override
-//    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//            String textSpinner = parent.getItemAtPosition(position).toString();
-//            Toast.makeText(parent.getContext(), textSpinner, Toast.LENGTH_SHORT).show();
-//    }
-//
-//    @Override
-//    public void onNothingSelected(AdapterView<?> parent) {
-//
-//    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        selectedCity = parent.getItemAtPosition(position).toString();
+    }
+
+    private void calculateTime() {
+        Toast.makeText(getContext(), "Calculating time of " + selectedCity, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
